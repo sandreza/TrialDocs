d1736c75be7a6e722089faa01669a4438303246b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-03-28 06:17:48 +0000

    build based on 3b29ee2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Example-1","page":"Example","title":"Example","text":"","category":"section"},{"location":"#","page":"Example","title":"Example","text":"Example Julia package repo.","category":"page"},{"location":"#","page":"Example","title":"Example","text":"Modules = [TrialDocs]","category":"page"},{"location":"#TrialDocs.domath-Tuple{Number}","page":"Example","title":"TrialDocs.domath","text":"domath(x::Number)\n\nReturn x + 5.\n\n\n\n\n\n","category":"method"},{"location":"#TrialDocs.hello-Tuple{String}","page":"Example","title":"TrialDocs.hello","text":"hello(who::String)\n\nReturn \"Hello, who\".\n\n\n\n\n\n","category":"method"}]
+}
