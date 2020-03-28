5f6a8713cd0dcbca9037898bb2473de4c28766ab
sandreza <andrenogueirasouza@gmail.com>
2020-03-28 10:24:10 -0400

    changing name to see how to do this

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, TrialDocs
 
-makedocs(modules = [TrialDocs], sitename = "TrialDocs")
+makedocs(modules = [TrialDocs], sitename = "TrialDocs2")
 
 deploydocs(repo = "github.com/sandreza/TrialDocs.git")
