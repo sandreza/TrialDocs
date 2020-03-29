31a593efb3369ef43ef0df5e81a7d0147d034d73
sandreza <andrenogueirasouza@gmail.com>
2020-03-29 19:27:39 -0400

    attempt 1

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,16 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, Literate, TrialDocs
 
+
+const examples_directory = pwd() * "/src/example/"
+const output_directory = pwd() * "/src/generated/"
 # should generate on the fly
 examples = ["literate_example.jl"]
+
+for example in examples
+    example_filepath = examples_directory * example
+    Literate.markdown(example_filepath, output_directory, documenter = true)
+end
 
 makedocs(
     modules = [TrialDocs],
@@ -12,6 +20,9 @@ makedocs(
             "Iterative Solvers" => "iterative_solvers/contribution_guide.md",
             "Time Stepping" => "time_steppers/contribution_guide.md",
         ],
+        "Examples" => [
+            "first example" => "generated/literate_example.jl"
+        ]
         "Function Index" => "function_index.md"
     ],
     sitename = "TrialDocs"
