1de21793854f1b737b002bd07a053e97007062b1
sandreza <andrenogueirasouza@gmail.com>
2020-03-29 20:35:42 -0400

    let's see if we can plot without the env var

--- docs/example/literate_example.jl
+++ docs/example/literate_example.jl
@@ -3,7 +3,7 @@
 # Here we will try to create a literate example that is also code
 
 # First we import our module
-using TrialDocs
+using TrialDocs, Plots
 
 # Now we use the functions in the module
 # Specifically the testargs function
@@ -13,3 +13,6 @@ test_args_kw(1, "tomorrow", ones(2,2), keyword = "my keyword")
 # ```math
 #   \frac{1}{2}
 # ```
+
+x = randn(3)
+plot(x)
